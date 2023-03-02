7a494e175a395db8613399b31cadd9302d32650d
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-03-01 20:33:10 -0600

    passing all tests for this equation

--- src/power.js
+++ src/power.js
@@ -5,21 +5,33 @@
  * @param {integer} exponent a non-negative integer
  */
 
-// given two integers a and b
-// b not negative   Math.sign(exponent) returns 1 or neg 1
-// a raised to the power of b  Math.pow(base, exponent)
-// multiplied by itself b times  
-// a is called base
-// b is called the exponent
+/*given two integers a and b
+b not negative   Math.sign(exponent) returns 1 or neg 1
+a raised to the power of b  Math.pow(base, exponent)
+multiplied by itself b times
+a is called base
+b is called the exponent
+*/
 function power(base, exponent) {
-    // top down
-    if(Math.sign(exponent)=== -1){
-        throw 
-    }
+  // top down
+  if (Math.sign(exponent) === -1) {
+    throw Error("exponent should be >= 0");
+  }
+
+  //   base phase
+  if (exponent === 0) {
+    return 1;
+  }
+
+  // Math.pow(base, exponent);
+  // recursive phase
+  return base * power(base, exponent - 1);
 }
 
-console.log(power(5, 10))
-console.log(power(5, -10));
-
-
+console.log(power(5, 5));
+//return 3,125
+console.log(power(5, 2));
+//return 25
+console.log(power(10, 2));
+// return 100
 module.exports = power;
